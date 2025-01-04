dcd43e1680038801cdc38060b14c5d1e5c633459
osama seid <osaseid1@gmail.com>
2025-01-04 11:28:42 +0300

    urls.py and viewa.py updated

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,7 +5,7 @@ from snippets import views
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
-
+    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
